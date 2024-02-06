7a7eb4d804d03d98c7ef359dc220a4aaf725f69a
Lonecoder.cro <25649334+lonecoder-cro@users.noreply.github.com>
2024-02-06 15:41:58 -0500

    Add files via vscode

--- .ts/utils.d.ts
+++
@@ -1,7 +0,0 @@
-import { EventEmitter } from "./core";
-export declare class HTMALEmitter extends EventEmitter {
-}
-export declare function htamlEvalHCode(code: string): Promise<unknown>;
-export declare function poll(fn: any, cb: any, iv: any, tm: any): void;
-export declare function htamlGet(url: string, async: boolean | undefined, config: any): Promise<unknown>;
-//# sourceMappingURL=utils.d.ts.map
