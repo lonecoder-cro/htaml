6ff3cbe312c7d8f86887eeea1e883b8aad9c266c
lonecoder-cro <25649334+lonecoder-cro@users.noreply.github.com>
2024-02-19 17:57:34 -0500

    Add files via vscode

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 
 const CONFIG = {
-    FOR_LOOP_REGEX: /(([a-zA-Z_-]+)\sin\s[.a-zA-Z_-]+|([a-zA-Z_-]+=[+-/*'$0-9a-zA-Z_-]+))/gi,
+    FOR_LOOP_REGEX: /(([a-zA-Z_-]+)\sin\s[.a-zA-Z_-]+|([a-zA-Z_-]+=[+-/*'0-9a-zA-Z_-]+))/gi,
     DISABLED_ELEMENTS: ['FORM', 'BUTTON', 'INPUT'],
-    MODIFIERS_REGEX: /(?<=\s|^)([a-zA-Z_]+:\S+)(?=\s|$)/gi,
+    MODIFIERS_REGEX: /([a-zA-Z_]+:[a-zA-Z_]+)/gi,
     HTAML_REGEX: /(h|ht|hta|htaml)-(\w*):(\w*)/,
     H_SCRIPT: 'HSCRIPT'
 
